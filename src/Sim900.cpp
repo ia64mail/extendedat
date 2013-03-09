d5d3c2a156751abcc2f4a2acca17319a1bf6a505
Ihor Cherednichenko <icherednichenko@ksu.ks.ua>
2013-03-09 21:17:58 +0200

    Enhanced logic to communicate with UART

--- src/Sim900.cpp
+++ src/Sim900.cpp
@@ -14,7 +14,19 @@
 #include <errno.h>   /* Error number definitions */
 #include <termios.h> /* POSIX terminal control definitions */
 
+#define DEBUG
 #define COM_PORT "/dev/ttyUSB0"
+
+//Total number of attempts communicate with port
+const unsigned short TOTAL_TRIES = 3;
+
+//Size of temp buffer for cleaning port in case receiving buffer overflow
+const unsigned short TEMP_BUFFER_SIZE = 10;
+
+//Control chars redefinitions
+const char CHAR_CR = '\r';
+const char CHAR_NL = '\n';
+const char CHAR_TR = '\0';
 
 using namespace std;
 
@@ -134,112 +146,212 @@ int init_modem(int fd) {
 	return (-1);
 }
 
-/*
- * Send test commands to modem
+/**
+ * Write buffer to output. Control chars will be encoded.
  */
-int send_test_commands() {
-	cout << "Test commands ..." << endl;
+void writeEncoded(const char * const buffer) {
+	const char * buffPtr = buffer;
+	while (*buffPtr != CHAR_TR) {
+		if(iscntrl(*buffPtr)) {
+			switch(*buffPtr) {
+				case CHAR_NL : cout << "\\n";
+				break;
+				case CHAR_CR : cout << "\\r";
+				break;
+				default: cout << '\\' << int(*buffPtr);
+				break;
+			}
+		} else {
+			cout << *buffPtr;
+		}
 
-	return (-1);
+		buffPtr++;
+	}
 }
-
 
 /**
  * Send an AT command to UART port.
  * Command should be assembled with final <CR> key code.
  *
- * @param fd UART open port descriptor
+ * @param fd UART port descriptor
  *
- * @return 0 if send was successful,
- * -1 if sending data has integrity problem, 1 if send was error
+ * @return count of sent bytes if send was successful,
+ * or -1 if sending data has integrity problem
  */
-int sendUART(const int &fd, const char * const data, const int size) {
-	#ifdef Debug
-		cout << "Send to UART :" << data << " ";
+int sendUART(const int &fd, const char * const buffer) {
+	const char *buffPtr; /* Current char in buffer */
+	int nBytes; /* Number of bytes read */
+	int totalBytes = 0; /* Number of total bytes read */
+
+	//get size of buffer string excluding last terminate key code '\0'
+	int size = 0;
+	buffPtr = buffer;
+	while (*buffPtr != CHAR_TR) {
+		size++;
+		buffPtr++;
+	}
+	totalBytes = size;
+
+	#ifdef DEBUG
+		cout << "Send to UART: \"";
+		writeEncoded(buffer);
+		cout << "\" in " << totalBytes << " byte(s)";
 	#endif
 
 	//check data integrity
-	if(data == NULL
-			|| *(data + size - 1) != '\0'
-					|| *(data + size - 2) != '\n') {
-
-		#ifdef Debug
-				cout << "[Error! Data integrity failed!]" << endl;
+	if(buffer == NULL
+			|| *(buffer + totalBytes - 1) != CHAR_CR) {
+		#ifdef DEBUG
+			cout << endl << "... [Error] Data integrity failed!" << endl;
 		#endif
-
 		return -1;
 	}
 
 	//send AT command
-	for (int tries = 0; tries < 3; tries++) {
-		if (write(fd, data, size) < size) {
-			continue;
-		} else {
-			#ifdef Debug
-					cout << "[OK]" << endl;
-			#endif
+	//try few times to ensure that all data sent
+	buffPtr = buffer;
+	for (int tries = 0; tries < TOTAL_TRIES; tries++) {
+		while ((nBytes = write(fd, buffPtr, buffer - buffPtr + size)) > 0) {
+			buffPtr += nBytes;
+			size -= nBytes;
 
-			return 0;
+			//return if all buffer sent
+			if(size == 0) {
+				#ifdef DEBUG
+					cout << " [OK]" << endl;
+				#endif
+				return totalBytes;
+			}
 		}
 	}
 
-	#ifdef Debug
-			cout << "[Error! Communication failed!]" << endl;
+	#ifdef DEBUG
+		cout << endl << "... [Error] Communication failed!";
+		cout << "Actual size of sent data is " << totalBytes - size << " byte(s)"<< endl;
 	#endif
-	return 1;
+	return -1;
 }
 
 /**
  * Receive AT response from UART port.
  * Size of buffer should include +1 byte for null terminated key code.
  *
- * @return 0 if receive was successful, -1 if not enough size for incoming buffer
+ * @return count of received bytes if receive was successful,
+ *  -1 if not enough size for incoming buffer
  */
 int receiveUART(const int &fd, char * const buffer, const int size) {
-	char *bufptr; /* Current char in buffer */
-	int nbytes; /* Number of bytes read */
+	char *buffPtr; /* Current char in buffer */
+	int nBytes; /* Number of bytes read */
+	int totalBytes = 0; /* Number of total bytes read */
 
-	#ifdef Debug
-		cout << "Receive from UART :";
+	#ifdef DEBUG
+		cout << "Receive from UART: ";
 	#endif
 
 	//receive AT response
-	bufptr = buffer;
-	while ((nbytes = read(fd, bufptr, buffer - bufptr + size - 1)) > 0) {
-		bufptr += nbytes;
-		if (bufptr[-1] == '\n' || bufptr[-1] == '\r') {
+	//try few times to ensure that all data received
+	buffPtr = buffer;
+	for (int tries = 0; tries < TOTAL_TRIES; tries++) {
+		//read from port until 0 bytes received
+		while ((nBytes = read(fd, buffPtr, buffer - buffPtr + size - 1)) > 0) {
+			buffPtr += nBytes;
+			totalBytes += nBytes;
+		}
+
+		//break if buffer already full
+		if(buffer - buffPtr == size - 1) {
 			break;
 		}
 	}
 
-	if (bufptr[-1] == '\n' || bufptr[-1] == '\r') {
-		/* null terminate the string and see if we got an OK response */
-		*bufptr = '\0';
 
-		#ifdef Debug
-			cout << bufptr << " "<<"[OK]" << endl;
-		#endif
+	/* null terminate the string and see if we got an OK response */
+	*buffPtr = CHAR_TR;
 
-		return 0;
+	#ifdef DEBUG
+			cout << "\"";
+			writeEncoded(buffer);
+			cout << "\"";
+			cout << " in " << totalBytes << " byte(s)";
+	#endif
+
+	//ensure, that all data received and buffer is empty
+	bool isOverloaded = false;
+	if(buffPtr - buffer == size-1) {
+		int tempSize = 10;
+		char * temp = new char[tempSize];
+		for (int tries = 0; tries < TOTAL_TRIES; tries++) {
+			while ((nBytes = read(fd, temp, tempSize)) > 0) {
+				isOverloaded = true;
+				totalBytes += nBytes;
+			}
+		}
+
+		if(isOverloaded) {
+			#ifdef DEBUG
+				cout << endl << "... [Error] Buffer overloaded! ";
+				cout << "Actual size of received data is " << totalBytes << " byte(s)"<< endl;
+			#endif
+			return -1;
+		}
 	}
 
-	#ifdef Debug
-			cout << bufptr << " " << "[Error! Buffer overloaded]" << endl;
+	if (buffPtr[-1] == CHAR_NL && buffPtr[-2] == CHAR_CR) {
+		#ifdef DEBUG
+				cout << " [OK]" << endl;
+		#endif
+
+		return totalBytes;
+	}
+
+	#ifdef DEBUG
+		cout <<  endl << "... [Error] Data integrity failed!" << endl;
 	#endif
 	return -1;
 }
 
-int main() {
-	#ifdef Debug
-	cout << "Starting..." << endl;
+void configureUART(const int &fd){
+	#ifdef DEBUG
+		cout << endl << "Configuring UART ..." << endl;
 	#endif
 
+	bool initFlag = true;
+
+	char * buffer;
+	unsigned short size;
+
+	//Configure ECHO mode
+	if(sendUART(fd, "ATE0\r") > 0) {
+		size = 10;
+		buffer = new char[size];
+		initFlag &= (receiveUART(fd, buffer, size) > 0);
+		delete buffer;
+	}
+}
+
+/**
+ * Main entry point
+ */
+int main() {
 	int fd = open_port();
 
-	sendUART(fd, "AT\n", 3);
 
-	char buffer[3];
-	receiveUART(fd, buffer, 3);
+	configureUART(fd);
+
+	char * buffer;
+	#ifdef DEBUG
+		cout << endl << "Communicating..." << endl;
+	#endif
+
+	buffer = new char[400];
+	sendUART(fd, "AT&V\r");
+	receiveUART(fd, buffer, 400);
+	delete buffer;
+
+	buffer = new char[30];
+	sendUART(fd, "AT\r");
+	receiveUART(fd, buffer, 30);
+	delete buffer;
 
 	return 0;
 }
